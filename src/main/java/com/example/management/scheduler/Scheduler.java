eadc8f74a90961527ad0d6da1c01feeb05e25dbc
Achraf <achraf.arboun@esprit.tn>
2020-06-23 12:33:22 +0200

    [Maiia-test] [1.0.0] Scheduler - add message collection job

--- src/main/java/com/example/management/scheduler/Scheduler.java
+++ src/main/java/com/example/management/scheduler/Scheduler.java
@@ -1,11 +1,15 @@
 package com.example.management.scheduler;
 
 
+import com.example.management.model.Message;
 import com.example.management.services.MessageService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 
 @Component
@@ -16,6 +20,18 @@ public class Scheduler {
 
     @Scheduled(cron = "1 * * * * ?")
     public void cronJobSch() {
+
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+        Date now = new Date();
+        String strDate = sdf.format(now);
+        System.out.println("Java cron job expression:: " + strDate);
+
+        final String uri = "https://jsonplaceholder.typicode.com/posts";
+
+        RestTemplate restTemplate = new RestTemplate();
+        Message[] result = restTemplate.getForObject(uri, Message[].class);
+        assert result != null;
+        messageService.saveMessage(result);
 
     }
 }
